2a194d698d920c854a0b5e2cab258c865dbf3fe2
Utkal Keshari Barik <49710146+utkalgituser@users.noreply.github.com>
2022-08-17 07:53:14 +0530

    Create Testdemo.java

---
+++ src/main/java/com/qa/openkart/pages/Testdemo.java
@@ -0,0 +1,13 @@
+package com.qa.openkart.pages;
+
+public class Testdemo {
+
+	public void f1() {
+		System.out.println("Testdemo feature");
+	}
+	public static void main(String[] args) {
+		System.out.println("main method Testdemo");
+	
+	}
+
+}
